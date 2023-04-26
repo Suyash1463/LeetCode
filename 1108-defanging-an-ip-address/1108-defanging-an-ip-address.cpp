e03b2502953b7c005900ed678ff1e45c0b4918e6
Suyash Shinde <suyashsshinde1463@gmail.com>
2023-04-26 23:58:48 +0530

    Time: 0 ms (100.00%), Space: 6 MB (74.49%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        stringstream ss;
+        string defanged;
+        defanged.reserve(address.size() + 6); // Add 6 to account for "[.]" in place of each period
         for (char c : address) {
             if (c == '.') {
-                ss << "[.]";
+                defanged += "[.]";
             } else {
-                ss << c;
+                defanged += c;
             }
         }
-        return ss.str();
+        return defanged;
     }
 };
